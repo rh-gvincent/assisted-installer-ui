db13f82c5822392d077690f6b5502dbc761d089a
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-02 08:56:39 +0200

    organizes the tsconfigs

--- apps/assisted-ui/src/env.d.ts
+++ apps/assisted-ui/src/env.d.ts
@@ -1,10 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly TRANSLATION_NAMESPACE: string;
-  readonly VITE_APP_API_ROOT: string;
-  readonly VITE_APP_API_URL: string;
-  readonly VITE_APP_CLUSTER_PERMISSIONS: string;
   readonly VITE_APP_GIT_SHA: string;
   readonly VITE_APP_IMAGE_REPO: string;
   readonly VITE_APP_VERSION: string;
