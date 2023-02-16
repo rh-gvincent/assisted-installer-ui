cfdafe1fa80f78cbd1a08fac457c950fa2509ede
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-16 17:36:23 +0200

    [WIP] deprecates transform.package-manifest script
    Compiles in uhc-portal but locales are broken

--- libs/openshift-assisted-ui-lib/vite.config.ts
+++ libs/openshift-assisted-ui-lib/vite.config.ts
@@ -1,42 +1,39 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import { externalizeDeps } from 'vite-plugin-externalize-deps';
 import dts from 'vite-plugin-dts'
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
-import pkgManifest from "./package.json";
+import packageJson from './package.json';
+import tsconfig from './tsconfig.json';
 
 export default defineConfig({
   build: {
     emptyOutDir: true,
     lib: {
       entry: {
-        'index': path.resolve("./src/index.ts"),
-        'cim/index': path.resolve("./src/cim/index.ts"),
-        'ocm/index': path.resolve("./src/ocm/index.ts"),
+        'index': path.resolve(packageJson.source),
+        // 'cim/index': path.resolve("./src/cim/index.ts"),
+        // 'ocm/index': path.resolve("./src/ocm/index.ts"),
       },
-      formats: ['es'],
+      formats: ['cjs'],
     },
-    minify: false,
-    rollupOptions: {
-      external: (id: string) => Object.keys(pkgManifest.peerDependencies).some(dep => new RegExp(dep).test(id)),
-      output: {
-        sourcemapExcludeSources: true,
-      },
-    },
-    outDir: "build/lib",
-    sourcemap: true,
+    minify: false,    
+    // rollupOptions: {
+    //   output: {
+    //     exports: 'named',
+    //     preserveModules: true,
+    //     preserveModulesRoot: 'src',
+    //     sourcemapExcludeSources: true,
+    //   },
+    // },
+    outDir: tsconfig.compilerOptions.outDir,
+    sourcemap: process.env.NODE_ENV === 'production' ? 'hidden' : 'inline',
   },
   plugins: [
-    react(),
-    dts({
-      skipDiagnostics: true
+    externalizeDeps({
+      peerDeps: true,
     }),
-    chunkSplitPlugin({
-      strategy: "all-in-one",
-      customSplitting: {
-        'common/index': [/src\/common/],
-        'deps': [/node_modules/]
-      }
-    })
+    dts({
+      skipDiagnostics: true,
+    }),
   ],
 });
