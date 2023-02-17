8b029a244c959ca54eb8940e2319c0b9c4eca2db
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-17 11:26:45 +0200

    deprecates the prepreview in assisted-ui

--- apps/assisted-ui/scripts/vite-wrapper.sh
+++ apps/assisted-ui/scripts/vite-wrapper.sh
@@ -9,4 +9,10 @@ if [[ "$command" =~ ^(preview|serve)$ ]];then
   export "$(grep -E '^AIUI_APP_API_URL' .env.development.local)"
 fi
 
+if [[ "$command" =~ ^(preview)$ ]] && [ ! -d  build ]; then
+    yarn build
+else
+  echo 'Previewing files using the existing '\''build'\'' directory'
+fi
+
 yarn vite "$command" -c vite.config.ts
