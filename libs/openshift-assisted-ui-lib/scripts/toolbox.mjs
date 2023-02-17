e55f27cbe8f3802b67ae0e2093f51c5ca8e12615
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-17 12:28:55 +0200

    [WIP] fixes for the uhc-portal
    Fixes:
    - Fixes the build in uhc-portal
    - In uhc-portal only, strings locales are not displayed correctly for components located in the lib's common folder
    squashme

--- libs/openshift-assisted-ui-lib/scripts/toolbox.mjs
+++ libs/openshift-assisted-ui-lib/scripts/toolbox.mjs
@@ -1,14 +1,39 @@
+/// <reference types="node">
+
 import fs from 'node:fs';
-import url from 'nbode:url';
+import url from 'node:url';
+import path from 'node:path';
+import glob from 'glob';
 
-export function legacyFilename() {
-    return url.fileURLToPath(import.meta.url); 
+/**
+ * @param {string} file - A path to a JSON file.
+ * @returns {unknown}
+ */
+export function loadJSON(file) {
+    const fileUrl = new URL(file, import.meta.url);
+    return JSON.parse(fs.readFileSync(fileUrl).toString());
 }
 
-export function legacyDirname() {
-    return url.fileURLToPath(new URL('.', import.meta.url)); 
-}
-
-export function loadJSON(path) {
-    return JSON.parse(fs.readFileSync(new URL(path, import.meta.url)).toString());
+/**
+ * @param {string} globPattern
+ * @param {string} sourcesRoot 
+ * @param {string} importMetaUrl 
+ * @returns 
+ */
+export function genenrateEntriesFromSources(globPattern, sourcesRoot, importMetaUrl) {
+    return Object.fromEntries(
+		glob.sync(globPattern).map(file => {
+            // This remove `src/` and the file extension from each
+            // file, so e.g. src/nested/foo.js becomes nested/foo
+            const entryAlias = path.relative(
+                sourcesRoot,
+                file.slice(0, file.length - path.extname(file).length)
+            );
+            // This expands the relative paths to absolute paths, so e.g.
+            // src/nested/foo becomes /project/src/nested/foo.js
+            const filePath = url.fileURLToPath(new URL(file, importMetaUrl))
+            
+            return [entryAlias, filePath];
+        }),
+	);
 }
--- libs/openshift-assisted-ui-lib/vite.config.ts
+++ libs/openshift-assisted-ui-lib/vite.config.ts
@@ -1,39 +1,39 @@
-import path from 'node:path';
 import { defineConfig } from 'vite';
 import { externalizeDeps } from 'vite-plugin-externalize-deps';
 import dts from 'vite-plugin-dts'
-import packageJson from './package.json';
 import tsconfig from './tsconfig.json';
+import { genenrateEntriesFromSources } from './scripts/toolbox.mjs';
 
-export default defineConfig({
+const entry = genenrateEntriesFromSources(
+  'src/**/*.!(yaml)',
+  tsconfig.compilerOptions.rootDir,
+  import.meta.url
+);
+
+export default defineConfig(({ mode }) => ({
   build: {
     emptyOutDir: true,
     lib: {
-      entry: {
-        'index': path.resolve(packageJson.source),
-        // 'cim/index': path.resolve("./src/cim/index.ts"),
-        // 'ocm/index': path.resolve("./src/ocm/index.ts"),
-      },
+      entry,
       formats: ['cjs'],
     },
     minify: false,    
-    // rollupOptions: {
-    //   output: {
-    //     exports: 'named',
-    //     preserveModules: true,
-    //     preserveModulesRoot: 'src',
-    //     sourcemapExcludeSources: true,
-    //   },
-    // },
     outDir: tsconfig.compilerOptions.outDir,
-    sourcemap: process.env.NODE_ENV === 'production' ? 'hidden' : 'inline',
+    rollupOptions: {
+      output: {
+        exports: 'named',
+        sourcemapExcludeSources: true,
+      },
+    },
+    sourcemap: mode === 'production' ? 'hidden' : true,
   },
   plugins: [
     externalizeDeps({
+      deps: false,
       peerDeps: true,
     }),
     dts({
       skipDiagnostics: true,
     }),
   ],
-});
+}));
