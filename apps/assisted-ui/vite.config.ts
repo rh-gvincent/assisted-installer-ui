2e3774f9af403dbe801dcb7de208e660d8852ec9
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-19 19:14:51 +0200

    emptyOutDir in assisted-ui

--- apps/assisted-ui/vite.config.ts
+++ apps/assisted-ui/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), envVarsPrefix,);
   return {
     build: {
+      emptyOutDir: true,
       outDir: 'build',
       sourcemap: 'inline'
     },
