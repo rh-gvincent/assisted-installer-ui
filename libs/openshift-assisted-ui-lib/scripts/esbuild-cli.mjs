df34cf70df4e43dc500dd43182b10316bb27a2ab
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-20 21:26:13 +0200

    Fixes the esbuild-cli watch mode

--- libs/openshift-assisted-ui-lib/scripts/esbuild-cli.mjs
+++ libs/openshift-assisted-ui-lib/scripts/esbuild-cli.mjs
@@ -1,58 +1,45 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+///<reference types="esbuild">
+
 import 'zx/globals';
 import esbuild from 'esbuild';
 
 
-/** @param {import('esbuild').BuildResult} buildResult */
-const handleBuildSucceeded = (buildResult) => {
-  console.log('Build succeeded');
-  if (buildResult.errors?.length > 0) {
-    console.error(...buildResult.errors);
-  }
-  
-  if (buildResult.warnings?.length > 0) {
-    console.warn(...buildResult.warnings);
-  }
-  
-  if (buildResult.outputFiles?.length > 0) {
-    console.info(...buildResult.outputFiles.map(({ path }) => path));
-  }
-};
+/**
+ * @param {esbuild.BuildContext} [ctx]
+ * @returns {NodeJS.SignalsListener}
+ */
+const handleInterrupt = (ctx = null) => {
+  return (signal) => {
+    console.error(`\n${signal} recevied, stop watching files...`);
+    if (!ctx) {
+      process.exit(1);
+    }
+    
+    ctx.dispose().then(() => process.exit(2));
+  };
+}
 
 async function main() {
   const pwd = process.cwd();
   const esbuildConfigFile = argv['c'];
-  const isWatchMode = argv['watch']
-
-  const { default: esbuildConfig } = await import(path.resolve(pwd, esbuildConfigFile));
+  const isWatchMode = argv['watch'];
+  
+  /** @type {esbuild.BuildOptions} */
+  const esbuildConfig = (await import(path.resolve(pwd, esbuildConfigFile))).default;
   if (fs.existsSync(esbuildConfig.outdir)) {
     fs.rmSync(esbuildConfig.outdir, { recursive: true });
   }
-
-  try {
-    let buildResult = null;
-    if (isWatchMode) {
-      buildResult = esbuild.build({
-        ...esbuildConfig,
-        watch: {
-          onRebuild(error, result) {
-            if (error) {
-              console.error('Watch build failed:', error);
-            } else {
-              console.log('Watch build succeeded:', result);
-            }
-          },
-        },
-      });
-      console.log('Build succeeded and started watching', buildResult)
-    } else {
-      buildResult = esbuild.build(esbuildConfig);
-    }
-
-    handleBuildSucceeded(buildResult);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
+  
+  if (isWatchMode) {
+    process.once('SIGINT', handleInterrupt());
+    const ctx = await esbuild.context(esbuildConfig); 
+    process.once('SIGINT', handleInterrupt(ctx));
+    await ctx.watch();
+    console.log('Build succeeded, watching files...');
+  } else {
+    console.log('Building files...');
+    await esbuild.build(esbuildConfig);
+    console.log('Build succeeded');
   }
 }
 
