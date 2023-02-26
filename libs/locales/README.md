b848f78bf1a903eccbfb81e064a73f44900e5ef5
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-26 20:19:53 +0200

    [UNSTABLE] Introduces the libs workspace
    - This is an unstable commit, some dependencies will be satisfied in later commits.
    - Moves `src` to `libs/ui-lib` and `libs/locales`
    - Renames the UI library: `openshift-assisted-ui-lib` becomes `@openshift-assisted/ui-lib` and `@openshift-assisted/locales`

---
+++ libs/locales/README.md
@@ -0,0 +1,3 @@
+# Assisted Installer locales library
+
+[![NPM](https://img.shields.io/npm/v/@openshift-assisted/locales.svg)](https://www.npmjs.com/package/@openshift-assisted/locales)
---
+++ libs/locales/lib/index.js
@@ -0,0 +1,6 @@
+const msg = `
+This is a stub, consume the translation files according to @openshift-assisted/locales exports field.
+E.g.:
+import translation from '@openshift-assisted/locales/en/translation.json';
+`
+console.warn(msg);
