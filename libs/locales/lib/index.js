832aba2cb5c549f22070850ef4640a5ad27aed9a
Jonathan Kilzi <jkilzi@redhat.com>
2023-02-27 22:49:03 +0200

    Fixes the package exports in the locales pkg
    - Removes index.js and tsconfig.json
    - Adds content to README.md

--- libs/locales/lib/index.js
+++
@@ -1,6 +0,0 @@
-const msg = `
-This is a stub, consume the translation files according to @openshift-assisted/locales exports field.
-E.g.:
-import translation from '@openshift-assisted/locales/en/translation.json';
-`
-console.warn(msg);
