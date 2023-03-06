3e12dd523f2b714c9dc30dcba62db1c14d3c2713
Jonathan Kilzi <jkilzi@redhat.com>
2023-03-06 16:38:56 +0200

    Adapts the top-level scripts paths

--- scripts/i18n.sh
+++ scripts/i18n.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-yarn i18next \"libs/ui-lib/src/**/*.{js,jsx,ts,tsx}\" -c ./scripts/i18next-parser.config.js;
+yarn i18next \"libs/ui-lib/lib/**/*.{js,jsx,ts,tsx}\" -c ./scripts/i18next-parser.config.js;
 
 find libs/locales/lib -type f -exec sed -i 's#": "ai:#": "#' {} \;
--- scripts/update-api.sh
+++ scripts/update-api.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 SWAGGER_FILE="$1"
